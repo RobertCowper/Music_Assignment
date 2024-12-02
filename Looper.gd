b09c95e5a62aa244108b160faf00f81c91ed7e10
C24741505 Robert Cowper <C24741505@myTUDublin.ie>
2024-12-02 14:40:32 +0000

    looper

--- Looper.gd
+++ Looper.gd
@@ -1,23 +1,34 @@
 extends Node2D
 
+var pattern = []
+var steps = 16
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	num_samples = get_child_count() - 1
+	
+	for i in range(steps):
+		pattern.push_back(randi_range(0, num_samples -1))
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-var i:int
+var current:int
 var num_samples:int
 
+var dir = 1
+
 func _on_timer_timeout() -> void:
-	var s = get_child(i)
-	s.play()
-	i = i + 1
-	
-	if i == num_samples:
-		i = 0
+	var sn = pattern[current]
+	if sn != -1:
+		var s = get_child(sn)
+		s.play()
+		print(current)
+	current = current + dir
+	if current == steps:
+		current = current - 1 
+		dir =- dir
+	if current == -1:
+		current = 0
+		dir =- dir
 	
